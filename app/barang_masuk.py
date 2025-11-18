5fa03a7d68e90fde8d846db9d7d7a0fb620f8ab4
salmanurrdh <salmanurrdh@gmail.com>
2025-11-18 13:24:50 +0700

    memperbaiki

--- app/barang_masuk.py
+++ app/barang_masuk.py
@@ -1,22 +1,28 @@
-from app.database import get_all_inventory, update_inventory
+from database import get_all_inventory, update_inventory
 from app.tampilkan_barang import tampilkan_barang
 
 def barang_masuk():
-    print("\n--- Barang Masuk ---")
-    inventory = get_all_inventory()
-    tampilkan_barang()
-    
-    if not inventory:
-        return
-    
-    idx = int(input("Pilih nomor barang: ")) - 1
-    if idx < 0 or idx >= len(inventory):
-        print("Nomor barang tidak valid!")
-        return
+    inpt=input("Back?(b/c): ").lower
+    if inpt == "b":
+        exit()
+    else:
+        print("\n--- Barang Masuk ---")
+        inventory = get_all_inventory()
+        tampilkan_barang()
         
-    jumlah = int(input("Jumlah barang masuk: "))
-    item_id = inventory[idx]["id"]
-    new_jumlah = inventory[idx]["jumlah"] + jumlah
-    
-    update_inventory(item_id, new_jumlah)
-    print(f"Stok '{inventory[idx]['nama']}' bertambah sebanyak {jumlah}.\n")
+        if not inventory:
+            return
+        
+        idx = int(input("Pilih nomor barang: ")) - 1
+        if idx < 0 or idx >= len(inventory):
+            print("Nomor barang tidak valid!")
+            return
+            
+        jumlah = int(input("Jumlah barang masuk: "))
+        item_id = inventory[idx]["id"]
+        new_jumlah = inventory[idx]["jumlah"] + jumlah
+        
+        update_inventory(item_id, new_jumlah)
+        print(f"Stok '{inventory[idx]['nama']}' bertambah sebanyak {jumlah}.\n")
+while True:
+    barang_masuk()
